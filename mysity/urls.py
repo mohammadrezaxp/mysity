f20ab85bca4613bd624cbe50354c3d3c2350b6a9
mohammadreza <m.mahmoudi.xp@gmail.com>
2025-10-09 02:28:08 +0330

    add
    add STATICFILES_DIRS =[
    
        BASE_DIR / 'statics'
    
    ]

--- mysity/urls.py
+++ mysity/urls.py
@@ -1,7 +1,11 @@
 
 from django.urls import path
 from mysity.views import about , contact
+from django.conf import settings
+from django.conf.urls.static import static
 urlpatterns = [
     path('ansur/',about),
     path('jison/',contact)
 ]
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0])
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

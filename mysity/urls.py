954897cda8efeeb2e8f46d0707483a18a954b570
mohammadreza <m.mahmoudi.xp@gmail.com>
2025-10-10 15:40:35 +0330

    templeat
    add bade tampleat to my projeac

--- mysity/urls.py
+++ mysity/urls.py
@@ -1,11 +1,14 @@
 
 from django.urls import path
-from mysity.views import about , contact
+from mysity.views import  contact , home , about , blog_home , blog_single
 from django.conf import settings
 from django.conf.urls.static import static
 urlpatterns = [
-    path('ansur/',about),
-    path('jison/',contact)
+    path('home/', home, name='index'),
+    path('contact/',contact, name = 'contact'),
+    path('about/',about,name='about'),
+    path('blog_single',blog_single,name='blog_single'),
+    path('blog_home/',blog_home,name='blog_home')
 ]
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0])
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- mysity/views.py
+++ mysity/views.py
@@ -2,7 +2,15 @@ from  django.http import HttpResponse , JsonResponse
 from django.shortcuts import render
 
 
-def about (request):
-   return render(request,'indx.html')
+def home (request):
+    return render(request, 'mysity/index.html')
 def contact(request) :
-    return render(request,'about.html')
+    return render(request,'mysity/contact.html')
+def about (request) :
+    return render(request,'mysity/about.html')
+
+def blog_home (request) :
+    return render(request,'blog/blog-home.html')
+
+def blog_single (request) :
+    return render(request,'blog/blog-single.html')

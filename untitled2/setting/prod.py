4cf2cc04578cc9fbbc7540a275591d189fdc3e9d
mohammadreza <m.mahmoudi.xp@gmail.com>
2025-12-19 16:46:22 +0330

    host

--- untitled2/setting/prod.py
+++ untitled2/setting/prod.py
@@ -1,14 +1,12 @@
 from untitled2.settings import *
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-^#(*+$_7#)^ww-orc58au3z9v&9r4bzn*8cq=d7jklc=_4kyj!'
-
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
+ALLOWED_HOSTS = ["95.134.130.121", 'weplay.beauty']
 
-ALLOWED_HOSTS = ["95.134.130.121",'weplay.beauty']
+
+INSTALLED_APPS = [app for app in INSTALLED_APPS if app != 'debug_toolbar']
+MIDDLEWARE = [m for m in MIDDLEWARE if 'debug_toolbar' not in m]
 
 DATABASES = {
     'default': {
@@ -21,5 +19,6 @@ STATIC_ROOT = BASE_DIR / 'static'
 MEDIA_ROOT = BASE_DIR / 'media'
 STATICFILES_DIRS = [BASE_DIR / 'statics']
 
-CSRF_COOKIE_SECURE = True
-SESSION_COOKIE_SECURE = True
+
+CSRF_COOKIE_SECURE = False
+SESSION_COOKIE_SECURE = False
--- untitled2/settings.py
+++ untitled2/settings.py
@@ -15,6 +15,9 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+SECRET_KEY = 'django-insecure-^#(*+$_7#)^ww-orc58au3z9v&9r4bzn*8cq=d7jklc=_4kyj!'
+DEBUG = True
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
